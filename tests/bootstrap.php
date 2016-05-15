fe64b57e427fd7e7b30b1c18407f21cf0f18af94
binarystash <binarystash01@gmail.com>
2016-05-15 21:00:34 +0800

    Fixed bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../media-dimensions.php';
+	require dirname( __FILE__ ) . '/../wp-weather-shortcodes.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
@@ -14,5 +14,4 @@ require $_tests_dir . '/includes/bootstrap.php';
 
 //Load classes to be tested
 
-require_once MEDIA_DIMENSIONS_DIR . 'classes/class-media-dimensions-image.php';
-require_once MEDIA_DIMENSIONS_DIR . 'classes/class-media-dimensions-video.php';
+require_once WP_WEATHER_SHORTCODES_DIR . 'classes/class-wp-weather-shortcodes-provider.php';
